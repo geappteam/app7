3c5a6bad2818a69feeddd47afa751f6100c73596
Anthony Parris <para2709@usherbrooke.ca>
2017-11-28 13:51:04 -0500

    correct som stuff for validation

--- app7.m
+++ app7.m
@@ -6,9 +6,10 @@ disp('Identification de la salle (N obtenu par recherche binaire pour E < 0.5)..
 identification
 
 %% Réduction du bruit ambient
-disp('\n\n  Réduction du bruit ambient...');
+disp('Réduction du bruit ambient');
+addpath('ambientNoiseReduction');
 ambientNoiseReduction
 
 %% Transformee bilineaire
-disp('\n\n  Transformee bilineaire');
+disp('Transformee bilineaire');
 bilinearT
