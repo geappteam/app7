55211ca674da5800a44d5ce6ce92c67435e7120f
MoronixProduct3 <moronicproduct3@gmail.com>
2017-11-28 04:15:15 -0500

    Basic bilinear transform

--- bilinearT.m
+++ bilinearT.m
@@ -7,7 +7,8 @@ Fs = 16000;
 wc = fc*2*pi;
 
 % Filter to transfer to z domain
-fonctionEnS = tf(1,  [1/wc^2, sqrt(2)/wc, 1])
+cDen = [1/wc^2, sqrt(2)/wc, 1];
+fonctionEnS = tf(1,  cDen)
 bode(fonctionEnS)
 title('Analog filter')
 
@@ -19,4 +20,13 @@ freqz(expect_b,expect_a)
 title('Expected filter from butter()');
 
 %% Biliear transform
+T = 1/Fs;
+cT = 2/T;
+C = cDen(1) * cT^2 + cDen(2) * cT + 1;
 
+b = [1, 2, 1] / C
+a = [ 1 ,  (2 - cDen(1)* 2 * cT^2) / C,  (cDen(1) * cT^2 - cDen(2) * cT + 1)/ C]
+
+figure()
+freqz(b,a)
+title('Filter obtained with bilinear transform');
